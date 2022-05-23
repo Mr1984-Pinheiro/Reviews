eb48c8169620953a70d58580c7d5be0ce4001056
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-05-23 06:36:21 -0300

    React Icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
 import Review from './Review';
+
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    
+  <main>
+    <section className='container'>
+      <div className='title'>
+            <h2>el reviews</h2>
+            <div className='underline'></div>
+      </div>
+    </section>
+  </main>
+  
+  
+  );
 }
 
 export default App;
